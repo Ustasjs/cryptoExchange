45632150d5255193b5a4ab8f729de486f2fa75b3
Ustiuzhanin Stanislav <ov.s.ust@gmail.com>
2017-12-14 11:49:45 +0300

    add saga`s tests

---
+++ src/sagas/__test__/trade.test.js
@@ -0,0 +1,75 @@
+import {
+  sellCurrencyRequest,
+  sellCurrencySuccess,
+  sellCurrencyFailure,
+  buyCurrencyRequest,
+  buyCurrencySuccess,
+  buyCurrencyFailure
+} from '../../actions/trade';
+import { takeLatest, call, put } from 'redux-saga/effects';
+import { sellCurrency, buyCurrency } from '../../api';
+import { fetchSellCurrencySaga, fetchBuyCurrencySaga } from '../trade';
+
+describe('Saga fetchSellCurrencySaga', () => {
+  describe('Script without error', () => {
+    const action = { payload: { currency: 'btc', value: 1000 } };
+    const saga = fetchSellCurrencySaga(action);
+    it('1. Effect call sellCurrency', () => {
+      expect(saga.next().value).toEqual(
+        call(sellCurrency, action.payload.currency, action.payload.value)
+      );
+    });
+    it('2. Effect put sellCurrencySuccess', () => {
+      expect(saga.next({ data: 'data' }).value).toEqual(
+        put(sellCurrencySuccess('data'))
+      );
+    });
+  });
+
+  describe('Script with error', () => {
+    const action = { payload: { currency: 'btc', value: 1000 } };
+    const saga = fetchSellCurrencySaga(action);
+    it('1. Effect call sellCurrency', () => {
+      expect(saga.next().value).toEqual(
+        call(sellCurrency, action.payload.currency, action.payload.value)
+      );
+    });
+    it('2. Effect put sellCurrencyFailure', () => {
+      expect(saga.throw({ error: 'error' }).value).toEqual(
+        put(sellCurrencyFailure({ error: 'error' }))
+      );
+    });
+  });
+});
+
+describe('Saga fetchBuyCurrencySaga', () => {
+  describe('Script without error', () => {
+    const action = { payload: { currency: 'btc', value: 1000 } };
+    const saga = fetchBuyCurrencySaga(action);
+    it('1. Effect call buyCurrency', () => {
+      expect(saga.next().value).toEqual(
+        call(buyCurrency, action.payload.currency, action.payload.value)
+      );
+    });
+    it('2. Effect put buyCurrencySuccess', () => {
+      expect(saga.next({ data: 'data' }).value).toEqual(
+        put(buyCurrencySuccess('data'))
+      );
+    });
+  });
+
+  describe('Script with error', () => {
+    const action = { payload: { currency: 'btc', value: 1000 } };
+    const saga = fetchBuyCurrencySaga(action);
+    it('1. Effect call buyCurrency', () => {
+      expect(saga.next().value).toEqual(
+        call(buyCurrency, action.payload.currency, action.payload.value)
+      );
+    });
+    it('2. Effect put buyCurrencyFailure', () => {
+      expect(saga.throw({ error: 'error' }).value).toEqual(
+        put(buyCurrencyFailure({ error: 'error' }))
+      );
+    });
+  });
+});
